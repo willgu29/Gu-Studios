00f5ead6faec1dfb098fe07f4070b0c50e73ed5a
Will Gu <willgu29@gmail.com>
2015-12-27 16:27:04 -0800

    well.. I can send mail now

--- server/models/Follower.js
+++ server/models/Follower.js
@@ -3,15 +3,10 @@ var mongoose = require('mongoose');
 var followerSchema = new mongoose.Schema({
 
   email: String,
-  firstName: String,
-  lastName: String,
-  fullName: String,
-  interests : {
-  	tech: Boolean,
-  	writing: Boolean,
-  	philosophy: Boolean,
-  	random: Boolean
-  },
+  tech: Boolean,
+  writing: Boolean,
+  philosophy: Boolean,
+  random: Boolean,
   dateCreated: {type: Date, default: Date.now},
 
 });
--- server/server.js
+++ server/server.js
@@ -36,8 +36,81 @@ app.get("/gustudios/api/admin", auth, function (req, res) {
 });
 
 
+app.post("/gustudios/api/newFollower", function (req, res) {
+	var newFollower = new Follower({email: req.body.email,
+  									tech: req.body.tech,
+  									writing: req.body.writing,
+  									philosophy: req.body.philosophy,
+  									random: req.body.random
+  									});
+
+	newFollower.save(function (err, newFollower) {
+         if (err) {
+           console.error(err);
+           return res.json({info: "There was an error. Please try again in a minute."});
+          } else {
+
+           //TODO: Send welcome email here
+
+           return res.json("You're all set. Check your inbox for a special email!");
+          }
+     });
+});
+
+app.post("/gustudios/api/admin/sendMail", function (req, res) {
+	//Send mail
+  	 var toArray = req.body.to_email.split(",");
+  var formattedArray = []
+  for (var i =0; i < toArray.length; i++) {
+    var format = {
+      "email": toArray[i],
+      "type": "to"
+    };
+    formattedArray.push(format);
+  }
+
+
+  var tagsArray = req.body.tags.split(",");
+  console.log(JSON.stringify(toArray));
+  var message = {
+    "key": config.MANDRILL_MESSAGE_KEY,
+    "html": req.body.html,
+    "subject": req.body.subject,
+    "from_email": req.body.from_email,
+    "from_name": req.body.from_name,
+    "to": formattedArray,
+    "track_opens": true,
+    "track_clicks": true,
+    "auto_text": true,
+    "preserve_recipients": false,  //display recipients
+    "tags": tagsArray
+
+  };
+
+
+  mandrill_client.messages.send({"message": message}, function(result) {
+    console.log(result);
+    /*
+    [{
+            "email": "recipient.email@example.com",
+            "status": "sent",
+            "reject_reason": "hard-bounce",
+            "_id": "abc123abc123abc123abc123abc123"
+        }]
+    */
+    res.json(result);
+  }, function(e) {
+    // Mandrill returns the error as an object with name and message keys
+    res.json(e.message);
+    console.log('A mandrill error occurred: ' + e.name + ' - ' + e.message);
+    // A mandrill error occurred: Unknown_Subaccount - No subaccount exists with the id 'customer-123'
+  });
+  
+});
 //***************
 
+var mandrill = require('mandrill-api/mandrill');
+var mandrill_client = new mandrill.Mandrill(config.MANDRILL_KEY);
 
 
 /// MONGOOOSE Database Linking ****
@@ -54,4 +127,6 @@ db.once('open', function(callback) {
 });
 
 //***********************
+
+
 
